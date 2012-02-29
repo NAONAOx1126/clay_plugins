bbb8f0df5bac8063571e65143722eab3940efde0
Naohisa Minagawa <info@sweetberry.jp>
2012-03-01 00:55:44 +0900

    Reconstructクラスのクラス名の間違いを修正

--- clay_order/modules/Reconstruct.php
+++ clay_order/modules/Reconstruct.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * ### Order.Import
+ * ### Order.Reconstruct
  * 注文情報をインポートするためのクラスです。
  * PHP5.3以上での動作のみ保証しています。
  * 動作自体はPHP5.2以上から動作します。
@@ -13,7 +13,7 @@
  * @version   1.0.0
  * @param key インポートするファイルの形式を特定するためのキー
  */
-class Shop_Order_Reconstruct extends FrameworkModule{
+class Order_Reconstruct extends FrameworkModule{
 	function execute($params){
 		// ローダーを初期化
 		$loader = new PluginLoader("Order");
