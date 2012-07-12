41806bf6333414be3c9be61c0ba3d3e23c037637
Naohisa Minagawa <info@sweetberry.jp>
2012-07-12 18:03:44 +0900

    ポイントにユーザーIDが付加されるように変更

--- clay_member/batch/AutoRegisterMail.php
+++ clay_member/batch/AutoRegisterMail.php
@@ -159,6 +159,7 @@ class Member_AutoRegisterMail extends FrameworkModule{
 				// 新規登録時は登録ポイントを登録
 				$rule = $loader->loadModel("PointRuleModel");
 				$pointLog = $loader->loadModel("PointLogModel");
+				$_POST["customer_id"] = $customer->customer_id;
 				$pointLog->add($rule->getAddPoint(Member_PointRuleModel::RULE_ENTRY), $rule->getRuleName(Member_PointRuleModel::RULE_ENTRY), false);
 						
 				// エラーが無かった場合、処理をコミットする。
