3b85e3cc7fa80635c6528167405fe8dbc9586fb6
Naohisa Minagawa <info@clay-system.jp>
2012-07-31 22:34:32 +0900

    nullを適切に設定できるように修正

--- clay_member/tables/WelcomeSuggestsTable.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Member_WelcomeSuggestsTable extends DatabaseTable{
-	function __construct(){
-		$this->db = DBFactory::getConnection("member");
-		parent::__construct("member_welcome_suggests", "member");
-	}
-}
-?>
