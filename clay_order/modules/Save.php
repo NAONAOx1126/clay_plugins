5094ecd4d95cba9c2640864e027ed4cf89de97bf
Naohisa Minagawa <info@clay-system.jp>
2012-08-08 23:02:05 +0900

    注文入力時に金額をコメントに設定する。

--- clay_order/modules/Save.php
+++ clay_order/modules/Save.php
@@ -109,7 +109,7 @@ class Order_Save extends FrameworkModule{
 							// 購入時には購入金額に応じてポイント付与
 							$pointLog = $memberLoader->loadModel("PointLogModel");
 							$order->add_point = $rule->getAddPoint(Member_PointRuleModel::RULE_ORDER_SALES, ($order->subtotal > 0)?$order->subtotal:$order->total);
-							$pointLog->addCustomerRuledPoint($order->customer_id, $rule, Member_PointRuleModel::RULE_ORDER_SALES, ($order->subtotal > 0)?$order->subtotal:$order->total, null, $order->shop_comment);
+							$pointLog->addCustomerRuledPoint($order->customer_id, $rule, Member_PointRuleModel::RULE_ORDER_SALES, ($order->subtotal > 0)?$order->subtotal:$order->total, null, $order->shop_comment."(".(($order->subtotal > 0)?$order->subtotal:$order->total).")");
 						}
 
 						// エラーが無かった場合、処理をコミットする。
