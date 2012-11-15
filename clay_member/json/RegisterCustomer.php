063889634e37b6d2c9eec8db9c47f5fb970583ee
Naohisa Minagawa <info@clay-system.jp>
2012-11-16 08:40:13 +0900

    IDとメールアドレスの両方が空欄の場合、処理をスキップするように修正

--- clay_member/json/RegisterCustomer.php
+++ clay_member/json/RegisterCustomer.php
@@ -9,41 +9,43 @@ class Member_RegisterCustomer{
 		$loader->LoadSetting();
 		
 		$customer = $loader->loadModel("CustomerModel");
-		$customer->findByPrimaryKey($_POST["customer_id"]);
-		if(!($customer->customer_id > 0)){
-			// メールアドレスが一致した場合も同一ユーザーとみなす。
-			$customer->findByEmail($_POST["email"]);
-		}
-
-		foreach($_POST as $key => $value){
-			$customer->$key = $value;
-		}
-		// トランザクションの開始
-		Clay_Database_Factory::begin("member");
-		
-		try{
-			// 登録前の顧客IDを保持
-			$preCustomerId = $customer->customer_id;
-			
-			// 登録データの保存
-			$customer->save();
-			
-			// 新規登録時は登録ポイントを設定。
-			if(!($preCustomerId > 0)){
-				$rule = $loader->loadModel("PointRuleModel");
-				
-				// 新規登録時は登録ポイントを登録
-				$pointLog = $loader->loadModel("PointLogModel");
-				$pointLog->addCustomerRuledPoint($customer->customer_id, $rule, Member_PointRuleModel::RULE_ENTRY);
+		if(!empty($_POST["customer_id"]) || !empty($_POST["email"])){
+			$customer->findByPrimaryKey($_POST["customer_id"]);
+			if(!($customer->customer_id > 0)){
+				// メールアドレスが一致した場合も同一ユーザーとみなす。
+				$customer->findByEmail($_POST["email"]);
 			}
+	
+			foreach($_POST as $key => $value){
+				$customer->$key = $value;
+			}
+			// トランザクションの開始
+			Clay_Database_Factory::begin("member");
 			
-			$customer->findByPrimaryKey($customer->customer_id);
-			
-			// エラーが無かった場合、処理をコミットする。
-			Clay_Database_Factory::commit("member");
+			try{
+				// 登録前の顧客IDを保持
+				$preCustomerId = $customer->customer_id;
 				
-		}catch(Exception $ex){
-			Clay_Database_Factory::rollback("member");
+				// 登録データの保存
+				$customer->save();
+				
+				// 新規登録時は登録ポイントを設定。
+				if(!($preCustomerId > 0)){
+					$rule = $loader->loadModel("PointRuleModel");
+					
+					// 新規登録時は登録ポイントを登録
+					$pointLog = $loader->loadModel("PointLogModel");
+					$pointLog->addCustomerRuledPoint($customer->customer_id, $rule, Member_PointRuleModel::RULE_ENTRY);
+				}
+				
+				$customer->findByPrimaryKey($customer->customer_id);
+				
+				// エラーが無かった場合、処理をコミットする。
+				Clay_Database_Factory::commit("member");
+					
+			}catch(Exception $ex){
+				Clay_Database_Factory::rollback("member");
+			}
 		}
 		return $customer->toArray();
 	}
