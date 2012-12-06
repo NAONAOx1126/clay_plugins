7a3c3a60f55be8862dba34c770ffae5ef31d76d3
Naohisa Minagawa <info@clay-system.jp>
2012-12-06 14:54:05 +0900

    ツリー時に3000件ずつ取得するように修正

--- clay_content/modules/ActivePage/ProductList.php
+++ clay_content/modules/ActivePage/ProductList.php
@@ -72,13 +72,20 @@ class Content_ActivePage_ProductList extends Clay_Plugin_Module{
 				$_SERVER["ATTRIBUTES"][$params->get("result", "products")] = $result->products;
 			}elseif($params->check("tree")){
 				// トップページ用
-				$result = $select->fetch();
+				$index = 0;
+				$select->setLimit(3000, $index);
+				$list = $select->execute();
 				$_SERVER["ATTRIBUTES"][$params->get("result", "product_tree")] = array();
-				while(($item = $result->next()) !== FALSE){
-					if(!isset($_SERVER["ATTRIBUTES"][$params->get("result", "product_tree")][$item["category1"]][$item["category2"]][$item["category3"]])){
-						$_SERVER["ATTRIBUTES"][$params->get("result", "product_tree")][$item["category1"]][$item["category2"]][$item["category3"]] = array();
+				while(!empty($list)){
+					foreach($list as $item){
+						if(!isset($_SERVER["ATTRIBUTES"][$params->get("result", "product_tree")][$item["category1"]][$item["category2"]][$item["category3"]])){
+							$_SERVER["ATTRIBUTES"][$params->get("result", "product_tree")][$item["category1"]][$item["category2"]][$item["category3"]] = array();
+						}
+						$_SERVER["ATTRIBUTES"][$params->get("result", "product_tree")][$item["category1"]][$item["category2"]][$item["category3"]][] = $item["product_code"];
 					}
-					$_SERVER["ATTRIBUTES"][$params->get("result", "product_tree")][$item["category1"]][$item["category2"]][$item["category3"]][] = $item["product_code"];
+					$index += 3000;
+					$select->setLimit(3000, $index);
+					$list = $select->execute();
 				}
 			}else{
 				// トップページ用
